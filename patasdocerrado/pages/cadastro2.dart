d736d80794ec486fb43bc98d8a76208c9aacb4c4
NeerdyBee <116850945+NeerdyBee@users.noreply.github.com>
2024-10-03 22:26:27 -0300

    Telas de Cadastro - Luis

--- patasdocerrado/pages/cadastro2.dart
+++ patasdocerrado/pages/cadastro2.dart
@@ -45,9 +45,25 @@ class MainApp extends StatelessWidget {
             ),
             //IMAGEM - BEM VINDO E BOTÃO
             body: Column(children: [
+              CircleAvatar(
+                backgroundColor: Color(0xffE6E6E6),
+                radius: 50,
+                child: Icon(
+                  Icons.person,
+                  size: 90,
+                  color: Color(0xffCCCCCC),
+                ),
+              ),
               RichText(
                   text: TextSpan(
-                      text: 'Nome',
+                      text: 'Insira uma foto de perfil\n',
+                      style: TextStyle(
+                        fontSize: 16,
+                        height: 2,
+                      ))),
+              RichText(
+                  text: TextSpan(
+                      text: 'Nome de Usuário',
                       style: TextStyle(
                         fontSize: 16,
                         height: 2,
@@ -59,13 +75,13 @@ class MainApp extends StatelessWidget {
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
-                    hintText: 'Digite seu nome',
+                    hintText: 'Digite seu nome de usuário',
                   ),
                 ),
               ),
               RichText(
                   text: TextSpan(
-                      text: 'CPF',
+                      text: 'Cidade',
                       style: TextStyle(
                         fontSize: 16,
                         height: 2,
@@ -77,82 +93,10 @@ class MainApp extends StatelessWidget {
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
-                    hintText: 'Digite seu CPF',
+                    hintText: 'Sua Cidade',
                   ),
                 ),
               ),
-              RichText(
-                  text: TextSpan(
-                      text: 'E-mail',
-                      style: TextStyle(
-                        fontSize: 16,
-                        height: 2,
-                      ))),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    hintText: 'Digite seu e-mail',
-                  ),
-                ),
-              ),
-              RichText(
-                  text: TextSpan(
-                      text: 'Telefone',
-                      style: TextStyle(
-                        fontSize: 16,
-                        height: 2,
-                      ))),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    hintText: 'Digite seu telefone',
-                  ),
-                ),
-              ),
-              RichText(
-                  text: TextSpan(
-                      text: 'Senha',
-                      style: TextStyle(
-                        fontSize: 16,
-                        height: 2,
-                      ))),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    hintText: 'Senha',
-                  ),
-                ),
-              ),
-              RichText(
-                  text: TextSpan(
-                      text: 'Confirmar senha',
-                      style: TextStyle(
-                        fontSize: 16,
-                        height: 2,
-                      ))),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    hintText: 'Confirmar senha',
-                  ),
-                ),
-              )
             ])));
   }
 }
