71b464d0d57b64c7af88559dfdbdb3dc44ac2507
Ramiro V <118458898+ramiroVieiraa@users.noreply.github.com>
2024-10-10 18:04:26 -0300

    Update hometeste.dart

--- patasdocerrado/lib/pages/hometeste.dart
+++ patasdocerrado/lib/pages/hometeste.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 void main() {
   runApp(MyApp());
@@ -19,97 +20,125 @@ class PetCardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                children: [
-                  Image.asset(
-                    'logo.png',
-                    width: 93,
-                    height: 54,
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Image.asset(
+                          'logo.png',
+                          width: 93,
+                          height: 54,
+                        ),
+                      ),
+                      Spacer(),
+                      IconButton(
+                        icon: Icon(Icons.notifications_outlined),
+                        onPressed: () {},
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Bem vindo!',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Conheça seu novo melhor amigo!',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16),
+                Expanded(
+                  child: Stack(
+                    children: [
+                      ListView.builder(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 8.0),
+                        itemCount: 6,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 20),
+                            child: PetCard(imageName: 'dog0${index + 1}.jpg'),
+                          );
+                        },
+                      ),
+                      _buildFadeOverlay(top: true),
+                      _buildFadeOverlay(top: false),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Text(
-              'Bem vindo!',
-              style: TextStyle(
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.w500,
-                fontSize: 16,
-                color: Colors.black,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Conheça seu novo melhor amigo!',
-              style: TextStyle(
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.w700,
-                fontSize: 14,
-                color: Colors.black,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 16),
-            Expanded(
-              child: ListView(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                children: [
-                  buildCard('dog01.png'),
-                  SizedBox(height: 20),
-                  buildCard('dog02.png'),
-                  SizedBox(height: 20),
-                  buildCard('dog03.png'),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: Container(
-        margin: EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 8,
-              offset: Offset(0, -1),
-            ),
-          ],
-        ),
-        child: BottomNavigationBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Início',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.pets),
-              label: 'Adotáveis',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Perfil',
-            ),
-          ],
-          selectedItemColor: Color(0xFFFF623E),
-          unselectedItemColor: Color(0xFF6e6f76),
-        ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: CustomBottomNavigationBar(),
+          ),
+        ],
       ),
     );
   }
 
-  Widget buildCard(String imageName) {
+  Widget _buildFadeOverlay({required bool top}) {
+    return Positioned(
+      top: top ? 0 : null,
+      bottom: top ? null : 0,
+      left: 0,
+      right: 0,
+      child: IgnorePointer(
+        child: Container(
+          height: 15.0,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: top ? Alignment.topCenter : Alignment.bottomCenter,
+              end: top ? Alignment.bottomCenter : Alignment.topCenter,
+              colors: [
+                Colors.white,
+                Colors.white.withOpacity(0.0),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PetCard extends StatelessWidget {
+  final String imageName;
+
+  const PetCard({required this.imageName});
+
+  @override
+  Widget build(BuildContext context) {
     return Stack(
       alignment: Alignment.bottomCenter,
       children: [
@@ -117,126 +146,180 @@ class PetCardScreen extends StatelessWidget {
           width: 322,
           height: 368,
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
+            borderRadius: BorderRadius.circular(20),
             image: DecorationImage(
               image: AssetImage(imageName),
               fit: BoxFit.cover,
             ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 5,
+                spreadRadius: 2,
+                offset: Offset(0, 5),
+              ),
+            ],
           ),
         ),
         Positioned(
           bottom: 0,
-          child: Container(
-            width: 322,
-            height: 96,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 8,
-                ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Bartho',
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w700,
-                          fontSize: 14,
-                          color: Color(0xFFFF623E),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 123,
-                        height: 31,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color(0xFFFF623E),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                          child: Text(
-                            'Me adote!',
-                            style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.w500,
-                              fontSize: 11,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+          child: _buildBlurEffect(),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBlurEffect() {
+    return ClipRRect(
+      borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+        child: CardDetails(),
+      ),
+    );
+  }
+}
+
+class CardDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 322,
+      height: 96,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.75),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Bartho',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w700,
+                    fontSize: 14,
+                    color: Color(0xFFFF623E),
                   ),
-                  Text(
-                    'Rialma, Goias',
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFF623E),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text(
+                    'Me adote!',
                     style: TextStyle(
                       fontFamily: 'Poppins',
                       fontWeight: FontWeight.w500,
                       fontSize: 11,
-                      color: Color(0xFF8d8d8d),
+                      color: Colors.white,
                     ),
                   ),
-                  SizedBox(height: 8),
-                  Row(
-                    children: [
-                      Row(
-                        children: [
-                          Icon(Icons.male, size: 11, color: Color(0xFF8d8d8d)),
-                          SizedBox(width: 4),
-                          Text(
-                            'Macho',
-                            style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.w500,
-                              fontSize: 11,
-                              color: Color(0xFF8d8d8d),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(width: 16),
-                      Row(
-                        children: [
-                          Icon(Icons.access_time,
-                              size: 11, color: Color(0xFF8d8d8d)),
-                          SizedBox(width: 4),
-                          Text(
-                            '2 Anos',
-                            style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.w500,
-                              fontSize: 11,
-                              color: Color(0xFF8d8d8d),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
+                ),
+              ],
+            ),
+            Text(
+              'Rialma, Goias',
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w500,
+                fontSize: 11,
+                color: Color(0xFF8d8d8d),
               ),
             ),
+            SizedBox(height: 8),
+            Row(
+              children: [
+                InfoRow(icon: Icons.male, label: 'Macho'),
+                SizedBox(width: 16),
+                InfoRow(icon: Icons.access_time, label: '2 Anos'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InfoRow extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const InfoRow({required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(icon, size: 11, color: Color(0xFF8d8d8d)),
+        SizedBox(width: 4),
+        Text(
+          label,
+          style: TextStyle(
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w500,
+            fontSize: 11,
+            color: Color(0xFF8d8d8d),
           ),
         ),
       ],
     );
   }
 }
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+      padding: EdgeInsets.only(top: 4.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 8,
+            offset: Offset(0, -1),
+          ),
+        ],
+      ),
+      child: BottomNavigationBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Início',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Adotáveis',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+        ],
+        selectedItemColor: Color(0xFFFF623E),
+        unselectedItemColor: Color(0xFF6e6f76),
+      ),
+    );
+  }
+}
+
 
