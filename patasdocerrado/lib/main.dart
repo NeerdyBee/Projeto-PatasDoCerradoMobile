42ecf7f3316f999694df0c21d9eb3d095b63093e
NeerdyBee <116850945+NeerdyBee@users.noreply.github.com>
2024-10-18 20:31:55 -0300

    Integração com Back4App - Luis

--- patasdocerrado/lib/main.dart
+++ patasdocerrado/lib/main.dart
@@ -8,8 +8,15 @@ import 'package:patasdocerrado/pages/register.dart';
 import 'package:patasdocerrado/pages/recoverPswd.dart';
 import 'package:patasdocerrado/pages/recoverPswdCode.dart';
 import 'package:patasdocerrado/pages/recoverPswdNew.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  const keyApplicationId = 'BS70Hl8xgdpi1EQGC3BqWKOZBoAwSqH2erMgpg6z';
+  const keyClientKey = '9MJ87n7THIRB5ueD60MR4nsG2JYJ6og5USoMFnM8';
+  const keyParseServerUrl = 'https://parseapi.back4app.com';
+  await Parse().initialize(keyApplicationId, keyParseServerUrl,
+      clientKey: keyClientKey, debug: true);
   runApp(const MainApp());
 }
 
